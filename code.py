17c10a4c56b1e9e64e871fd1aaff08b91180ad70
Snehadeep Gupta <111756866+sngupta123@users.noreply.github.com>
2023-11-30 20:09:57 +0530

    Added Conversation History using Buffer Memory

--- code.py
+++ code.py
@@ -1,19 +1,24 @@
 import os
 from pathlib import Path
 from PIL import Image
+from Prompts.prompt import template
 from app_secrets import OPENAI_API_KEY
 from sql_execution import execute_sf_query
 import streamlit as st
-from langchain.prompts import load_prompt
-from langchain import OpenAI, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain import OpenAI, LLMChain, ConversationChain
+from langchain.memory import ConversationBufferMemory
 
 # create env variable
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+memory = ConversationBufferMemory(memory_key='history', return_message=True)
 root_path = [p for p in Path(__file__).parents if p.parts[-1] == "SQL Generator"][0]
 
 # create front-end using streamlit
 st.title("🦜🔗Langchain AI SQL Assistant")
-user_input = st.text_input("Enter your question here")
+user_input = st.text_input("You: ",
+                           placeholder="Your AI assistant here! Ask me anything ...",
+                           label_visibility='hidden')
 tab_titles = ["Result", "Query", "ER Diagram"]
 tabs = st.tabs(tab_titles)
 
@@ -22,17 +27,34 @@ erd_image = Image.open(f'{root_path}/images/employees.jpg')
 with tabs[2]:
     st.image(erd_image)
 
+# initialize session state
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+else:
+    for message in st.session_state.chat_history:
+        memory.save_context({'input': message['human']}, {'output': message['AI']})
+
 # create the prompt
-prompt_template = load_prompt(f'{root_path}/Prompts/tpch_prompt.yaml')
+prompt_template = PromptTemplate(input_variables=['history', 'input'],
+                                 template=template)
 
 llm = OpenAI(temperature=0.1)
-sql_generation_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True)
+
+sql_generation_chain = LLMChain(llm=llm,
+                                prompt=prompt_template,
+                                memory=memory,
+                                verbose=True)
 
 if user_input:
     sql_query = sql_generation_chain(user_input)
     result = execute_sf_query(sql_query['text'])
+    message = {'human': user_input, 'AI': sql_query['text']}
+    st.session_state.chat_history.append(message)
+    st.write(sql_query)
     with tabs[0]:
         st.write(result)
     with tabs[1]:
         st.write(sql_query['text'])
+    with st.expander(label='Chat History', expanded=False):
+        st.write(st.session_state.chat_history)
 
